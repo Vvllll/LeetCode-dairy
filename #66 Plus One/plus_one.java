6cf1f74acc0f474ad1b1e284e600b48422ae5a21
Wei <vvllllw@gmail.com>
2020-06-01 16:38:30 +1000

    Update plus_one.java

--- #66 Plus One/plus_one.java
+++ #66 Plus One/plus_one.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         for(int i = digits.length-1; i>=0; i--){
+            // if it is 9, then become 0
+            // if it is not 9, then plus one
             if(digits[i] == 9){
                 digits[i] = 0;
             } else{
@@ -8,6 +10,7 @@ class Solution {
                 return digits;
             }
         }
+        // if all numbers are 9
         int[] result = new int[digits.length + 1];
         result[0] = 1;
         return result;
